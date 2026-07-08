d7b3f11440e37fa2752eb98e28ad715af996b9f6
shahidansari311 <shahidansari945256@gmail.com>
2026-07-08 14:02:00 +0530

    clerk

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Profile() {
   return (
+    <SafeAreaView>
+
     <View>
       <Text>profile</Text>
     </View>
+    </SafeAreaView>
   )
 }
--- app/(root)/(tabs)/saved.tsx
+++ app/(root)/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Saved() {
   return (
+    <SafeAreaView>
+
     <View>
       <Text>Saved</Text>
     </View>
+    </SafeAreaView>
   )
 }
